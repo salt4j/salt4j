7d4cf2ed78833df7a2f8dcb86d88ae23935405a3
Seun Osewa <seun.osewa@gmail.com>
2010-03-16 14:49:28 +0100

    tuples. left out of earlier commit; I don't know why

---
+++ salt4j/tuples/Tuple4.java
@@ -0,0 +1,6 @@
+package salt4j.tuples;
+
+public class Tuple4<A, B, C, D> {
+    final public A a;  final public B b;  final public C c; final public D d;
+    public Tuple4(A a, B b, C c, D d) { this.a = a; this.b = b; this.c = c; this.d = d; }
+}
