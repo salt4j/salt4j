6622ed727e564b7f675b2cdc138955468374f5f5
Seun Osewa <seun.osewa@gmail.com>
2010-04-23 23:27:01 +0100

    removed non-framework files

--- salt4j/tuples/Tuple2.java
+++
@@ -1,6 +0,0 @@
-package salt4j.tuples;
-
-public class Tuple2<A, B> {
-    final public A a;  final public B b;
-    public Tuple2(A a, B b) { this.a = a; this.b = b; }
-}
--- salt4j/tuples/Tuple3.java
+++
@@ -1,6 +0,0 @@
-package salt4j.tuples;
-
-public class Tuple3<A, B, C> {
-    final public A a;  final public B b;  final public C c;
-    public Tuple3(A a, B b, C c) { this.a = a; this.b = b; this.c = c; }
-}
